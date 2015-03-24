1804748d9badaecb2d38a2c313c6df1359462b33
interlinear <interlinear@gmail.com>
2015-03-24 13:17:59 +0100

    Check setting the key

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
-aws help
-#aws configure set aws_access_key_id $AWSKEY
+#aws help
+aws configure set aws_access_key_id $AWSKEY
 #aws configure set aws_secret_access_key $AWSSECRETKEY
 #aws configure set default.region us-west-2
 #aws configure set deafult.output json
