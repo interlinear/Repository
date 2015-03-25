6eced9388b3493d60b4efa2925816d6d033d0e8f
interlinear <interlinear@gmail.com>
2015-03-25 08:22:12 +0100

    Try copy of Dockerfile

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 aws --version
-aws configure set aws_access_key_id $AWSKEY
+#aws configure set aws_access_key_id $AWSKEY
 #aws configure set aws_secret_access_key $AWSSECRETKEY
 #aws configure set default.region us-west-2
 #aws configure set deafult.output json
 
 echo "copy Dockerfile"
-#aws s3 cp Dockerfile s3://$EB_BUCKET/Dockerfile
+aws s3 cp Dockerfile s3://$EB_BUCKET/Dockerfile
