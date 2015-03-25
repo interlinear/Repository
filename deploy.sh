72e719794c42374308e7771da225fbc9071b047e
interlinear <interlinear@gmail.com>
2015-03-25 09:53:20 +0100

    Run aws configure help to figure out what can be configured

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
-aws --version
+#aws --version
 #aws configure set aws_access_key_id $AWSKEY
 #aws configure set aws_secret_access_key $AWSSECRETKEY
 #aws configure set default.region us-west-2
 #aws configure set deafult.output json
 
-echo "copy Dockerfile"
+#echo "copy Dockerfile"
 #aws s3 ls s3://
-aws ec2 help
+aws configure help
