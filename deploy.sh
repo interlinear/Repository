83f92aecc90fee36e56e8214243e4c12f4863364
interlinear <interlinear@gmail.com>
2015-03-26 16:01:43 +0100

    aws first test

--- deploy.sh
+++ deploy.sh
@@ -1,22 +1,25 @@
-#aws --version
-#aws configure set aws_access_key_id $AWSKEY
-#aws configure set aws_secret_access_key $AWSSECRETKEY
-#aws configure set default.region us-west-2
-#aws configure set deafult.output json
-
-#echo "copy Dockerfile"
-#aws s3 ls s3://
-#aws configure help
-
 pip install --user awscli
 export PATH=$HOME/.local/bin:$PATH
-#aws configure set aws_access_key_id $AWSKEY
-#aws configure set aws_secret_access_key $AWSSECRETKEY
-#aws s3 ls s3://interlinear101
-pwd
-zip -r App.zip Dockerfile target/
-ls -la
-unzip -t App.zip
-aws configure set default.region us-west-2
-aws configure set default.output json
-aws elasticbeanstalk describe-applications
+#pwd
+#zip -r App.zip Dockerfile target/
+#ls -la
+#unzip -t App.zip
+#aws configure set default.region us-west-2
+#aws configure set default.output json
+#aws elasticbeanstalk describe-applications
+APPNAME="first-app"
+
+zip -r ${CIRCLE_SHA1}.zip Dockerfile target/
+aws s3 cp ${CIRCLE_SHA1}.zip s://$my_bucket/
+aws elasticbeanstalk create-application-version --application-name $APPNAME --version-label $CIRCLE_SHA1\
+    --description "My first elasticbeanstalk app in Docker container"\
+    --source-bundle S3Bucket=$mu_bucket,S3Key=${CIRCLE_SHA1}.zip\
+    --auto-create-application
+aws elasticbeanstalk create-configuration-template --application-name "first-app"\
+    --solution-stack-name "64bit Amazon Linux 2014.09 v1.2.1 running Docker 1.5.0"\
+    --template-name "first-app-template"
+aws elasticbeanstalk create-environment --application-name "first-app"\
+    --environment-name "first-app-docker"\
+    --template-name "first-app-template"\
+    --cname-prefix "cname-prefix"
+aws elasticbeanstalk update-environment --environment-name "first-app-docker" --version-label $CIRCLE_SHA1
