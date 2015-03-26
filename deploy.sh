ed4c13bcdc33d54d62978b406350835c19972327
interlinear <interlinear@gmail.com>
2015-03-26 17:01:55 +0100

    aws test tomcat don't create env nor conf template

--- deploy.sh
+++ deploy.sh
@@ -10,11 +10,11 @@ aws elasticbeanstalk create-application-version --application-name $APPNAME --ve
     --description "My first elasticbeanstalk app in Docker container"\
     --source-bundle S3Bucket=$my_bucket,S3Key=${CIRCLE_SHA1}.zip\
     --auto-create-application &&\
-aws elasticbeanstalk create-configuration-template --application-name "first-app"\
-    --solution-stack-name "64bit Amazon Linux 2014.09 v1.2.1 running Docker 1.5.0"\
-    --template-name "first-app-template" &&\
-aws elasticbeanstalk create-environment --application-name "first-app"\
-    --environment-name "first-app-docker"\
-    --template-name "first-app-template"\
-    --cname-prefix "cname-prefix" && \
+#aws elasticbeanstalk create-configuration-template --application-name "first-app"\
+#    --solution-stack-name "64bit Amazon Linux 2014.09 v1.2.1 running Docker 1.5.0"\
+#    --template-name "first-app-template" &&\
+#aws elasticbeanstalk create-environment --application-name "first-app"\
+#    --environment-name "first-app-docker"\
+#    --template-name "first-app-template"\
+#    --cname-prefix "cname-prefix" && \
 aws elasticbeanstalk update-environment --environment-name "first-app-docker" --version-label $CIRCLE_SHA1
