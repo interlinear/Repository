5f41c3519ef2ae2161752773b087f4f6b8bfdc4a
interlinear <interlinear@gmail.com>
2015-03-26 16:55:53 +0100

    aws test tomcat again

--- deploy.sh
+++ deploy.sh
@@ -8,13 +8,13 @@ zip -r ${CIRCLE_SHA1}.zip Dockerfile index.html
 aws s3 cp ${CIRCLE_SHA1}.zip s3://$my_bucket/
 aws elasticbeanstalk create-application-version --application-name $APPNAME --version-label $CIRCLE_SHA1\
     --description "My first elasticbeanstalk app in Docker container"\
-    --source-bundle S3Bucket=$mu_bucket,S3Key=${CIRCLE_SHA1}.zip\
-    --auto-create-application
+    --source-bundle S3Bucket=$my_bucket,S3Key=${CIRCLE_SHA1}.zip\
+    --auto-create-application &&\
 aws elasticbeanstalk create-configuration-template --application-name "first-app"\
     --solution-stack-name "64bit Amazon Linux 2014.09 v1.2.1 running Docker 1.5.0"\
-    --template-name "first-app-template"
+    --template-name "first-app-template" &&\
 aws elasticbeanstalk create-environment --application-name "first-app"\
     --environment-name "first-app-docker"\
     --template-name "first-app-template"\
-    --cname-prefix "cname-prefix"
+    --cname-prefix "cname-prefix" && \
 aws elasticbeanstalk update-environment --environment-name "first-app-docker" --version-label $CIRCLE_SHA1
