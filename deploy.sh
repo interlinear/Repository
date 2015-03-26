93c6c6acfafe25fdb10eb0f3f0b2e6f6e0416258
interlinear <interlinear@gmail.com>
2015-03-26 17:17:14 +0100

    add delay between commands

--- deploy.sh
+++ deploy.sh
@@ -2,19 +2,19 @@ pip install --user awscli
 export PATH=$HOME/.local/bin:$PATH
 aws configure set default.region us-west-2
 aws configure set default.output json
-APPNAME="first-app"
+APPNAME="lastappagain"
 
 zip -r ${CIRCLE_SHA1}.zip Dockerfile index.html
 aws s3 cp ${CIRCLE_SHA1}.zip s3://$my_bucket/
 aws elasticbeanstalk create-application-version --application-name $APPNAME --version-label $CIRCLE_SHA1\
     --description "My first elasticbeanstalk app in Docker container"\
     --source-bundle S3Bucket=$my_bucket,S3Key=${CIRCLE_SHA1}.zip\
-    --auto-create-application &&\
-#aws elasticbeanstalk create-configuration-template --application-name "first-app"\
-#    --solution-stack-name "64bit Amazon Linux 2014.09 v1.2.1 running Docker 1.5.0"\
-#    --template-name "first-app-template" &&\
-#aws elasticbeanstalk create-environment --application-name "first-app"\
-#    --environment-name "first-app-docker"\
-#    --template-name "first-app-template"\
-#    --cname-prefix "cname-prefix" && \
-aws elasticbeanstalk update-environment --environment-name "first-app-docker" --version-label $CIRCLE_SHA1
+    --auto-create-application && sleep 20  &&\
+aws elasticbeanstalk create-configuration-template --application-name $APPNAME\
+    --solution-stack-name "64bit Amazon Linux 2014.09 v1.2.1 running Docker 1.5.0"\
+    --template-name "my-app-template" && sleep 20 &&\
+aws elasticbeanstalk create-environment --application-name $APPNAME\
+    --environment-name "my-app-docker"\
+    --template-name "first-app-template"\
+    --cname-prefix "firstapp-test" && sleep 400 && \
+aws elasticbeanstalk update-environment --environment-name "my-app-docker" --version-label $CIRCLE_SHA1
